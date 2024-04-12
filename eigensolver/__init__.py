40eb3d338d4143f0a81e8d962980bbea6dd5f1ff
Qi Yang <qiyang@mail.ustc.edu.cn>
2024-04-12 22:13:49 +0800

    solved

--- eigensolver/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['eigs']
-
-from .interface import eigs
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import jax
 import jax.numpy as jnp
-from eigensolver import eigs
+from jaxeig import eigs
 from jax import config
 config.update("jax_enable_x64", True)
 
