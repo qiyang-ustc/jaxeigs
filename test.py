389a47c2bb50c580b39c5d53d7378454924d1f4b
Qi Yang <qiyang@mail.ustc.edu.cn>
2024-12-06 20:52:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import jax
 import jax.numpy as jnp
-from jaxeigs import eigs
+from jaxtn.solver.jaxeigs import eigs, eigsh
 from jax import config
 config.update("jax_enable_x64", True)
 
@@ -11,5 +11,16 @@ if __name__ == "__main__":
     def mapA(x): return A@x
     res = eigs(mapA, initial_state = b, numeig=1, num_krylov_vecs = 5)
     print(res[0],res[1][0])
-    
+
     A @ res[1][0] / res[1][0]
+
+
+    m = 10
+    A = jax.random.uniform(jax.random.PRNGKey(42),(m,m))
+    b = jax.random.uniform(jax.random.PRNGKey(41),(m,))
+    def mapA(x): return (A+A.T.conj())@x
+    res = eigsh(mapA, initial_state = b, numeig=1, num_krylov_vecs = 5)
+    print(res[0],res[1][0])
+
+    (A+A.T.conj()) @ res[1][0] / res[1][0]
+
